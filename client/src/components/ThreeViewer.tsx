b5661a21e5ae12359713ca894bf0abd9d673dd84
Tony Dye <38537906-tony206@users.noreply.replit.com>
2025-01-26 03:26:48 +0000

    Assistant checkpoint: Updated Spline 3D scene URL
    Assistant generated file changes:
    - client/src/components/ThreeViewer.tsx: Update Spline 3D scene URL
    
    ---
    
    User prompt:
    
    Please update the information used to display the Spline 3D scene in card 5
    https://my.spline.design/untitled-61fd32762a48cfe586b07aed3b895cbb/

--- client/src/components/ThreeViewer.tsx
+++ client/src/components/ThreeViewer.tsx
@@ -1,12 +1,13 @@
+
 export function ThreeViewer() {
   return (
     <div className="w-full h-full relative overflow-hidden">
       <iframe 
-        src='https://my.spline.design/untitled-61fd32762a48cfe586b07aed3b895cbb/' 
+        src='https://my.spline.design/sparqlogo3dscenefinal-68a89f0d7807b5e586b07aed3b895cbb/' 
         frameBorder='0' 
         width='100%' 
         height='100%'
-        title="Spline 3D Scene"
+        title="Sparq 3D Logo Scene"
         style={{
           position: 'absolute',
           top: 0,
