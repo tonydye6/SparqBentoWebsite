552bf8ef4e42164fc0c70c0d3c02b2b5d001d6d7
Tony Dye <38537906-tony206@users.noreply.replit.com>
2025-01-26 03:27:06 +0000

    Restored to '59fb7d2234c5d70417358a13ae184f84c615ba64'
    Replit-Restored-To:59fb7d2234c5d70417358a13ae184f84c615ba64

--- client/src/components/ThreeViewer.tsx
+++ client/src/components/ThreeViewer.tsx
@@ -1,13 +1,12 @@
-
 export function ThreeViewer() {
   return (
     <div className="w-full h-full relative overflow-hidden">
       <iframe 
-        src='https://my.spline.design/sparqlogo3dscenefinal-68a89f0d7807b5e586b07aed3b895cbb/' 
+        src='https://my.spline.design/untitled-61fd32762a48cfe586b07aed3b895cbb/' 
         frameBorder='0' 
         width='100%' 
         height='100%'
-        title="Sparq 3D Logo Scene"
+        title="Spline 3D Scene"
         style={{
           position: 'absolute',
           top: 0,
