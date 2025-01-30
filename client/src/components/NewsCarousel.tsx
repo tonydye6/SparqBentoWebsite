c3ab624c450a797fe9289c58cfe640586be4a852
Tony Dye <38537906-tony206@users.noreply.replit.com>
2025-01-30 04:17:47 +0000

    Assistant checkpoint: Adjust NewsCarousel height and padding
    Assistant generated file changes:
    - client/src/components/NewsCarousel.tsx: Adjust NewsCarousel height and spacing
    
    ---
    
    User prompt:
    
    reduced the height of the News Carousel component so that the top of the component doesn't interfere with the bottom of the Card title

--- client/src/components/NewsCarousel.tsx
+++ client/src/components/NewsCarousel.tsx
@@ -86,8 +86,8 @@ export function NewsCarousel() {
   }
 
   return (
-    <div className="h-[calc(100%-1rem)] p-2 flex flex-col">
-      <Card className="flex-1 p-3 bg-black/20 border-0">
+    <div className="h-[calc(100%-2rem)] p-1 flex flex-col">
+      <Card className="flex-1 p-2 bg-black/20 border-0">
         <div className="h-full flex flex-col">
           {news[currentIndex] && (
             <div 
